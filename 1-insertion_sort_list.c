60a9e850bd5aae8e82d2576dc4ac2ac21e2529e8
FredLenrie <fredlenrieb17@gmail.com>
2023-12-19 05:00:39 -0800

    task 0 and 1 updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,48 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-
-
-
-/**
- * insertion_sort_list - sorts a doubly linked list of
- * integers in ascending order
- * @list: A pointer to the head of the doubly linked list
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * prints the list after each time you swap two elements
  *
+ * @list: pointer to the linked list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *current, *next, *prev;
 
+	/* An list does not need to be sorted if its size is less than 2 */
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	current = (*list)->next;
+	while (current)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		next = current->next;
+
+		/* if 2 adjacent nodes are out of order */
+		while (current->prev && (current->n < current->prev->n))
 		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			/* swap current and current->prev */
+			prev = current->prev;
+
+			current->prev = prev->prev;
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;  /* update the head of the list */
+
+			prev->next = current->next;
+			if (prev->next)
+				prev->next->prev = prev;
+
+			current->next = prev;
+			prev->prev = current;
+
+			/* print list */
+			print_list(*list);
 		}
 
+		current = next;
 	}
-
-
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 **0x1B. C - Sorting algorithms & Big O**
 
-**Project By: Frederick Buadu and Obed Apkah**
+**Project By: Frederick Budu and Obed Apkah**
 
 **Tasks**
 
@@ -22,49 +22,3 @@ Function that sorts an array of integers in ascending order using the Selection 
 *3. Quick sort:*
 
 Function that sorts an array of integers in ascending order using the Quick sort algorithm
-
-
-*4. Shell sort - Knuth Sequence:*
-
-Function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth
-
-
-*5. Cocktail shaker sort:*
-
-function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm
-
-
-*6. Counting sort:*
-
-Function that sorts an array of integers in ascending order using the Counting sort algorithm
-
-
-*7. Merge sort:*
-
-Function that sorts an array of integers in ascending order using the Merge sort algorithm
-
-
-*8. Heap sort:*
-
-Function that sorts an array of integers in ascending order using the Heap sort algorithm
-
-
-*9. Radix sort:*
-
-function that sorts an array of integers in ascending order using the Radix sort algorithm
-
-
-*10. Bitonic sort:*
-
-function that sorts an array of integers in ascending order using the Bitonic sort algorithm
-
-
-*11. Quick Sort - Hoare Partition scheme:*
-
-function that sorts an array of integers in ascending order using the Quick sort algorithm
-
-
-*12. Dealer:*
-
-function that sorts a deck of cards.
-
